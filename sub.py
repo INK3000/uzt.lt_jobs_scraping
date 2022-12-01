1ab2dd33121da0c4a8a9c67784dd4bd3a9e2f345
ILYA KRIVENOK <ilya@krivenok.com>
2022-12-01 23:19:23 +0200

    add/remove subscribes

--- sub.py
+++ sub.py
@@ -1,31 +1,50 @@
+import json
 
 from models.database import Session
 from models.category import Category
 from models.user import User
 
 
-class Subscribes():
-    def __init__ (self, subscribes_d, categories):
-        self.added  = subscribes_d
-        self.not_added = self.get_not_added(categories)
+class Subscribes:
 
-    
+    def __init__(self, subscribes_d, categories):
+        self.added: dict = json.loads(subscribes_d)
+        self.not_added: dict = self.get_not_added(categories)
+
+    def __repr__(self):
+        return ','.join(list(sorted(self.added)))
+
     def get_not_added(self, categories):
-        all = {str(category.id): category.last_id for category in categories}
-        result = {key: val for key, val in all.items() if key not in self.added}
+        all_categories = {
+            str(category.id): category.last_id for category in categories}
+        result = {key: val for key, val in all_categories.items()
+                  if key not in self.added}
         return result
 
+    def update(self, text, oper):
+        to_update_list = self.text_to_list(text)
+        try:
+            match oper:
+                case '/add':
+                    self.add(to_update_list)
+                case '/remove':
+                    self.remove(to_update_list)
+        except StopIteration as ex:
+            print(f'Exception {ex}')
+            return False
+        return True
 
-    def update(self, input, oper):
-        to_add_list = self.text_to_list(input)
-         
-        pass
+    def remove(self, to_add_list):
+        for key in to_add_list:
+            value = self.added.pop(key, False)
+            if value:
+                self.not_added.update({key: value})
 
-
-    def remove(self, input):
-        to_add_list = self.text_to_list(input)
-        pass
-
+    def add(self, to_add_list):
+        for key in to_add_list:
+            value = self.not_added.pop(key, False)
+            if value:
+                self.added.update({key: value})
 
     @staticmethod
     def text_to_list(text):
@@ -38,8 +57,20 @@ def main():
         categories = session.query(Category).all()
         user = session.query(User).filter(User.id == 1).one_or_none()
         subscribes = Subscribes(user.subscribes, categories)
-        print(subscribes.not_added)
-        
+    while True:
+        answer = input('...$ ')
+        match answer:
+            case '/add' | '/remove':
+                text = input('укажите категории через запятую ')
+                resp = subscribes.update(text=text, oper=answer)
+                if resp:
+                    user.subscribes = json.dumps(subscribes.added)
+                    session.add(user)
+                    session.commit()
+            case '/show':
+                print(subscribes)
+            case '/quit':
+                break
 
 
 if __name__ == '__main__':
